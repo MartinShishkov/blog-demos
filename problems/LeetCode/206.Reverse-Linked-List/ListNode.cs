c5c9b45d3c42f3efc5546da166932ea1bac47fe0
MartinShishkov <martin@codeinsiders.net>
2020-07-13 11:31:15 +0300

    solution for reversing a linked list

---
+++ problems/LeetCode/206.Reverse-Linked-List/ListNode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _206.Reverse_Linked_List
+{
+    public class ListNode<T>
+    {
+        public ListNode(T val, ListNode<T> next = null)
+        {
+            this.Value = val;
+            this.Next = next;
+        }
+
+        public T Value { get; }
+
+        public ListNode<T> Next { get; set; }
+
+        public void Insert(T value)
+        {
+            if (this.Next == null)
+            {
+                this.Next = new ListNode<T>(value);
+            }
+            else
+            {
+                this.Next.Insert(value);
+            }
+        }
+
+        public void Print()
+        {
+            var p = this;
+            while (p != null)
+            {
+                Console.WriteLine(p.Value);
+                p = p.Next;
+            }
+        }
+    }
+}
---
+++ problems/LeetCode/206.Reverse-Linked-List/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _206.Reverse_Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new[] {1, 2, 3, 4, 5};
+
+            var list = ListFromArray(input);
+            list.Print();
+
+            var r = Reverse(list);
+            Console.WriteLine(r.Value);
+        }
+
+        static ListNode<int> ListFromArray(int[] array)
+        {
+            var head = new ListNode<int>(array[0]);
+            for (int i = 1; i < array.Length; i++)
+            {
+                head.Insert(array[i]);
+            }
+
+            return head;
+        }
+
+        static ListNode<int> Reverse(ListNode<int> head)
+        {
+            ListNode<int> prev = null;
+            ListNode<int> current = head;
+            ListNode<int> next = null;
+
+            while (current != null)
+            {
+                next = current.Next;
+                current.Next = prev;
+                prev = current;
+                current = next;
+            }
+
+            return prev;
+        }
+    }
+}

253786d118c2aa32c31a91cd146e67e0c9222ec5
MartinShishkov <martin@codeinsiders.net>
2020-07-14 14:39:01 +0300

    recursive linked list reverse

--- problems/LeetCode/206.Reverse-Linked-List/Program.cs
+++ problems/LeetCode/206.Reverse-Linked-List/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace _206.Reverse_Linked_List
+﻿namespace _206.Reverse_Linked_List
 {
     class Program
     {
@@ -9,10 +7,10 @@ namespace _206.Reverse_Linked_List
             var input = new[] {1, 2, 3, 4, 5};
 
             var list = ListFromArray(input);
-            list.Print();
+            //list.Print();
 
             var r = Reverse(list);
-            Console.WriteLine(r.Value);
+            r.Print();
         }
 
         static ListNode<int> ListFromArray(int[] array)
@@ -26,7 +24,7 @@ namespace _206.Reverse_Linked_List
             return head;
         }
 
-        static ListNode<int> Reverse(ListNode<int> head)
+        static ListNode<int> ReverseIterative(ListNode<int> head)
         {
             ListNode<int> prev = null;
             ListNode<int> current = head;
@@ -41,6 +39,23 @@ namespace _206.Reverse_Linked_List
             }
 
             return prev;
+        }
+
+        // recursive
+        static ListNode<int> Reverse(ListNode<int> head)
+        {
+            // we should be asking the code to reverse a smaller
+            // and smaller list until there's only 1 element left
+            // and the reversed version of that is itself
+
+            if (head == null || head.Next == null)
+                return head;
+
+            var reversed = Reverse(head.Next);
+            head.Next.Next = head;
+            head.Next = null;
+
+            return reversed;
         }
     }
 }

646877f04f31e250db26ec727ea0a184c012df98
MartinShishkov <martin@codeinsiders.net>
2020-07-15 11:46:08 +0300

    contains duplicate problem

---
+++ problems/LeetCode/217.Contains-Duplicate/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _217.Contains_Duplicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new int[] {1,1,1,3,3,4,3,2,4,2};
+            Console.WriteLine(ContainsDuplicate(input));
+        }
+
+        static bool ContainsDuplicate(int[] nums)
+        {
+            var set = new HashSet<int>(nums);
+
+            return set.Count != nums.Length;
+        }
+    }
+}
---
+++ problems/LeetCode/219.Contains-Duplicate-II/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _219.Contains_Duplicate_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
